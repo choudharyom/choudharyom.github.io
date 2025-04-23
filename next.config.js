5f862953a9af97efaeca9a2c8f9519607f1b6395
om choudhary <choudharyom@hotmail.com>
2025-04-23 12:29:57 +0000

    remove the assetPrefix configuration

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
+//const isProd = process.env.NODE_ENV === 'production';
 // Define assetPrefix based on environment
-const assetPrefix = isProd ? '/choudharyom-blog' : '';
+//const assetPrefix = isProd ? '/choudharyom-blog' : '';
 
 const nextConfig = {
   output: 'export',
@@ -23,14 +23,14 @@ const nextConfig = {
     ],
   },
   // Set the assetPrefix for Next.js internal use
-  assetPrefix: assetPrefix,
-  // Expose the assetPrefix to the client-side runtime
-  publicRuntimeConfig: {
-    assetPrefix: assetPrefix,
-  },
-  env: { // Add this block
-    NEXT_PUBLIC_ASSET_PREFIX: assetPrefix,
-  },
+  //assetPrefix: assetPrefix,
+  //// Expose the assetPrefix to the client-side runtime
+  //publicRuntimeConfig: {
+  //  assetPrefix: assetPrefix,
+  //},
+  //env: { // Add this block
+  //  NEXT_PUBLIC_ASSET_PREFIX: assetPrefix,
+  //},
   trailingSlash: true,
   // If using `next export`, ensure trailing slashes match GitHub Pages expectations
   // trailingSlash: true, // Often needed for GH Pages subdirectories
